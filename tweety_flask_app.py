43e3947a25bc99cfa2da5070bb8337c87e9ede0f
rubala19 <rubala19@users.noreply.github.com>
2015-12-22 15:06:53 -0800

    Create tweety_flask_app.py

---
+++ tweety_flask_app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Most used languages on Twitter: All Tweets"
+
+@app.route("/top_tweets")
+def top_tweets():
+    return "Most Popular Tweets"    
+
+@app.route("/trends")
+def trends():
+    return "Trending On Twitter:"
+
+if __name__ == "__main__":
+    app.run(debug = True)    
+Status API Training Shop Blog About Pricing
+© 2015 GitHub, Inc. Terms Privacy Security Contact Help
